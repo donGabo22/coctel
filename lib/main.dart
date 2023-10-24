e2e8cc017de8af721602ddc154daf142e979f009
Gabriel Hernandez Diaz <gabrielhdz266@gmail.com>
2023-10-23 22:51:15 -0600

    Cambio a Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,166 +1,18 @@
+import 'package:coctel/screens/screenMenu.dart';
 import 'package:flutter/material.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
-import 'package:line_icons/line_icons.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+  const MyApp({super.key});
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, this.title}) : super(key: key);
-
-  final String? title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int selectedIndex = 0;
-
-  List<GButton> tabs = [];
-  List<Color> colors = [
-    Colors.purple,
-    Colors.pink,
-    Colors.amber[600]!,
-    Colors.teal
-  ];
-
-  @override
-  void initState() {
-    super.initState();
-  }
+  static const appTitle = 'Drawer Demo';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: IndexedStack(
-        index: selectedIndex,
-        children: <Widget>[
-          buildPage(0),
-          buildPage(1),
-          buildPage(2),
-          buildPage(3),
-        ],
-      ),
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              spreadRadius: -10,
-              blurRadius: 60,
-              color: Colors.black.withOpacity(.20),
-              offset: const Offset(0, -15),
-            ),
-          ],
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-          child: GNav(
-            gap: 8,
-            color: Colors.grey[800],
-            activeColor: Colors.purple,
-            iconSize: 24,
-            tabBackgroundColor: Colors.purple.withOpacity(0.1),
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 14),
-            duration: const Duration(milliseconds: 1000),
-            tabs: const [
-              GButton(
-                icon: LineIcons.home,
-                text: 'Inicio',
-              ),
-              GButton(
-                icon: LineIcons.heart,
-                text: 'La selección de hoy',
-              ),
-              GButton(
-                icon: LineIcons.hotDog,
-                text: 'Comidas',
-              ),
-              GButton(
-                icon: LineIcons.search,
-                text: 'Buscar',
-              ),
-            ],
-            selectedIndex: selectedIndex,
-            onTabChange: (index) {
-              setState(() {
-                selectedIndex = index;
-              });
-            },
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget buildPage(int index) {
-    String pageTitle;
-    String pageContent;
-
-    if (index == 0) {
-      pageTitle = 'Inicio';
-      pageContent = '¡Buenos días!';
-      
-      
-    } else if (index == 1) {
-      pageTitle = 'La selección de hoy';
-      pageContent = 'Contenido de la página La selección de hoy';
-    } else if (index == 2) {
-      pageTitle = 'Comidas';
-      pageContent = 'Anuncio: Que rico!';
-    } else {
-      pageTitle = 'Buscar';
-      pageContent = 'Contenido de la página Buscar';
-    }
-
-    return AnimatedContainer(
-      duration: const Duration(milliseconds: 300),
-      color: colors[index],
-      child: Center(
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                spreadRadius: -10,
-                blurRadius: 60,
-                color: Colors.black.withOpacity(.20),
-                offset: const Offset(0, 15),
-              ),
-            ],
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 25),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Página: $pageTitle',
-                  style: const TextStyle(fontSize: 24),
-                ),
-                const SizedBox(height: 20),
-                Text(
-                  pageContent,
-                  style: const TextStyle(fontSize: 18),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+    return const MaterialApp(
+      title: appTitle,
+      home: MyHomePage(title: appTitle),
     );
   }
 }
---
+++ lib/providers/movies_provider.dart
@@ -0,0 +1,48 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:coctel/models/movie.dart';
+import 'package:coctel/models/now_playing_response.dart';
+import 'package:coctel/models/popular_response.dart';
+
+class MoviesProvider extends ChangeNotifier {
+  String _baseUrl = 'api.themoviedb.org';
+  String _apiKey = '9dc27117b000e7e5acfb365fa957971a';
+  String _language = 'es-MX';
+
+  List<Movie> onDisplayMovies = [];
+  List<Movie> popularMovies = [];
+
+  MoviesProvider() {
+    getOnDisplayMovies();
+    getPopularMovies();
+  }
+
+  getOnDisplayMovies() async {
+    var url = Uri.https(_baseUrl, '3/movie/now_playing',
+        {'api_key': _apiKey, 'language': _language, 'page': '1'});
+
+    final response = await http.get(url);
+    final Map<String, dynamic> decodeData = json.decode(response.body);
+    //print(decodeData);
+    //print(response.body);
+    final nowPLayingResponse = NowPlayingResponse.fromRawJson(response.body);
+    onDisplayMovies = nowPLayingResponse.results;
+    //Le comunicamos a todos los widgets que estan escuchando que se cambio la data por lo tanto se tienen que redibujar
+    notifyListeners();
+    print(nowPLayingResponse.results[0].title);
+  }
+
+  getPopularMovies() async {
+    var url = Uri.https(_baseUrl, '3/movie/popular',
+        {'api_key': _apiKey, 'language': _language, 'page': '1'});
+
+    final response = await http.get(url);
+
+    final popularResponse = PopularResponse.fromRawJson(response.body);
+
+    //Destructurar resultado para cambiar pagina y mantener los actuales
+    popularMovies = [...popularMovies, ...popularResponse.results];
+    notifyListeners();
+  }
+}
